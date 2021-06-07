fd03c634390c6d92315b541ef734a0ea704c9d87
Digitensions <pittshick@me.com>
2021-06-07 11:48:23 +0100

    More flock checks for second script collection

--- flock_rebuild.sh
+++ flock_rebuild.sh
@@ -4,11 +4,16 @@
 # CHECK IF FLOCK FILES EXIST, IF NOT RECREATE
 #############################################
 
-LOCKS=( "/var/run/dpx_assess.lock"
-        "/var/run/dpx_rawcook.lock"
-        "/var/run/dpx_post_rawcook.lock"
-        "/var/run/dpx_tar_script.lock"
-        "/var/run/dpx_clean_up.lock"
+LOCKS=( "/var/run/dpx_assess_filmop.lock"
+        "/var/run/dpx_assess_qnap.lock"
+        "/var/run/dpx_rawcook_filmop.lock"
+        "/var/run/dpx_rawcook_qnap.lock"
+        "/var/run/dpx_post_rawcook_filmop.lock"
+        "/var/run/dpx_post_rawcook_qnap.lock"
+        "/var/run/dpx_tar_script_filmop.lock"
+        "/var/run/dpx_tar_script_qnap.lock"
+        "/var/run/dpx_clean_up_filmop.lock"
+        "/var/run/dpx_clean_up_qnap.lock"
 )
 
 for lock in "${LOCKS[@]}" ; do
