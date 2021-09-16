6bf69479a36fcd6e16f15368e0647e0203205813
digitensions <pittshick@me.com>
2021-09-16 16:39:10 +0100

    Prepare for live use Film Ops

--- film_operations/dpx_assessment.sh
+++ film_operations/dpx_assessment.sh
@@ -48,8 +48,8 @@ find "${DPX_PATH}" -maxdepth 4 -mindepth 4 -type d -mmin +10 | while IFS= read -
             # Output metadata to filepath into second level folder
             log "Metadata file creation has started for:"
             log "- ${file_scan_name}/$reel/${dpx}"
-#            mediainfo -f "${files}/${dpx}" > "${DPX_PATH}${file_scan_name}/${filename}_${dpx}_metadata.txt"
-#            tree "${files}" > "${DPX_PATH}${file_scan_name}/${filename}_directory_contents.txt"
+            mediainfo -f "${files}/${dpx}" > "${DPX_PATH}${file_scan_name}/${filename}_${dpx}_metadata.txt"
+            tree "${files}" > "${DPX_PATH}${file_scan_name}/${filename}_directory_contents.txt"
 
             # Start comparison of first dpx file against mediaconch policy
             check=$(mediaconch --force -p "${POLICY_PATH}" "${files}/$dpx" | grep "pass!")
@@ -76,41 +76,49 @@ find "${DPX_PATH}" -maxdepth 4 -mindepth 4 -type d -mmin +10 | while IFS= read -
 done
 
 # Prepare luma_dpx_list for DPX splitting script/move to RAWcooked preservation
-log "Luma Y path items for size check and Python splitting/moving script:"
-list1=$(cat "${DPX_PATH}luma_dpx_list.txt" | rev | sort -n -k1.5 | rev )
-log "$list1"
-echo "$list1" > "${DPX_PATH}luma_dpx_list.txt"
-cat "${DPX_PATH}luma_dpx_list.txt" | while IFS= read -r line1; do
-  kb_size=$(du -s "$line1" | cut -f1)
-  log "Size of $line1 is $kb_size KB. Passing to Python script..."
-  echo "$kb_size, $line1, luma" >> "${DPX_PATH}python_list.txt";
-done
+if [ -s "${DPX_PATH}luma_dpx_list.txt" ]; then
+  log "Luma Y path items for size check and Python splitting/moving script:"
+  list1=$(cat "${DPX_PATH}luma_dpx_list.txt" | rev | sort -n -k1.5 | rev )
+  log "$list1"
+  echo "$list1" > "${DPX_PATH}luma_dpx_list.txt"
+  cat "${DPX_PATH}luma_dpx_list.txt" | while IFS= read -r line1; do
+    kb_size=$(du -s "$line1" | cut -f1)
+    log "Size of $line1 is $kb_size KB. Passing to Python script..."
+    echo "$kb_size, $line1, luma" >> "${DPX_PATH}python_list.txt";
+  done
+fi
 
 # Prepare tar_dpx_failure_list for DPX splitting script/move to TAR preservation
-log "TAR path items for size check and Python splitting/moving script:"
-list2=$(cat "${DPX_PATH}tar_dpx_list.txt" | rev | sort -n -k1.5 | rev )
-log "$list2"
-echo "$list2" > "${DPX_PATH}tar_dpx_list.txt"
-cat "${DPX_PATH}tar_dpx_list.txt" | while IFS= read -r line2; do
-  kb_size2=$(du -s "$line2" | cut -f1)
-  log "Size of $line2 is $kb_size2 KB. Passing to Python script..."
-  echo "$kb_size2, $line2, tar" >> "${DPX_PATH}python_list.txt";
-done
+if [ -s "${DPX_PATH}tar_dpx_list.txt" ]; then
+  log "TAR path items for size check and Python splitting/moving script:"
+  list2=$(cat "${DPX_PATH}tar_dpx_list.txt" | rev | sort -n -k1.5 | rev )
+  log "$list2"
+  echo "$list2" > "${DPX_PATH}tar_dpx_list.txt"
+  cat "${DPX_PATH}tar_dpx_list.txt" | while IFS= read -r line2; do
+    kb_size2=$(du -s "$line2" | cut -f1)
+    log "Size of $line2 is $kb_size2 KB. Passing to Python script..."
+    echo "$kb_size2, $line2, tar" >> "${DPX_PATH}python_list.txt";
+  done
+fi
 
 # Prepare dpx_success_list for DPX splitting script/move to RAWcooked preservation
-log "RAWcooked path items for size check and Python splitting/moving script:"
-list3=$(cat "${DPX_PATH}rawcooked_dpx_list.txt" | rev | sort -n -k1.5 | rev )
-log "$list3"
-echo "$list3" > "${DPX_PATH}rawcooked_dpx_list.txt"
-cat "${DPX_PATH}rawcooked_dpx_list.txt" | while IFS= read -r line3; do
-  kb_size3=$(du -s "$line3" | cut -f1)
-  log "Size of $line3 is $kb_size3 KB. Passing to Python script..."
-  echo "$kb_size3, $line3, rawcooked" >> "${DPX_PATH}python_list.txt";
-done
+if [ -s "${DPX_PATH}rawcooked_dpx_list.txt" ]; then
+  log "RAWcooked path items for size check and Python splitting/moving script:"
+  list3=$(cat "${DPX_PATH}rawcooked_dpx_list.txt" | rev | sort -n -k1.5 | rev )
+  log "$list3"
+  echo "$list3" > "${DPX_PATH}rawcooked_dpx_list.txt"
+  cat "${DPX_PATH}rawcooked_dpx_list.txt" | while IFS= read -r line3; do
+    kb_size3=$(du -s "$line3" | cut -f1)
+    log "Size of $line3 is $kb_size3 KB. Passing to Python script..."
+    echo "$kb_size3, $line3, rawcooked" >> "${DPX_PATH}python_list.txt";
+  done
+fi
 
 # Take python_list.txt and iterate through entries, passing to Python script
-log "Launching python script to process DPX sequences. Please see dpx_splitting_script.log for more details"
-grep '/mnt/' "${DPX_PATH}python_list.txt" | parallel --jobs 1 "$PY3_LAUNCH $SPLITTING {}"
+if [ -s "${DPX_PATH}python_list.txt" ]; then
+  log "Launching python script to process DPX sequences. Please see dpx_splitting_script.log for more details"
+  grep '/mnt/' "${DPX_PATH}python_list.txt" | parallel --jobs 1 "$PY3_LAUNCH $SPLITTING {}"
+fi
 
 # Append latest pass/failures to movement logs
 cat "${DPX_PATH}rawcooked_dpx_list.txt" >> "${DPX_PATH}rawcook_dpx_success.log"
