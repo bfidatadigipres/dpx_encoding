06735fdecb4135469db7b7e56d7a46e9ceb4cc6a
digitensions <pittshick@me.com>
2021-12-16 12:03:31 +0000

    Update check size scripts for more regular use monitoring 4K

--- check_size.sh
+++ check_size.sh
@@ -8,8 +8,11 @@
 INGEST="${QNAP_FILM}${AUTOINGEST_VID}"
 SIZE_LIST="${QNAP_FILM}size_list.txt"
 
+rm "${SIZE_LIST}"
+touch "${SIZE_LIST}"
+
 # Find all MKV files with newer modification time than supplied file
-find "${INGEST}" -name '*.mkv' -newer "${INGEST}N_4947690_01of02.mkv" | while IFS= read -r files; do
+find "${INGEST}" -name '*.mkv' -newer "${INGEST}N_4947690_01of02.mkv" | sort | while IFS= read -r files; do
     # Get kb size output to text file for comparison later
     kb_size=$(du -s "$files")
     echo "$kb_size" >> "${SIZE_LIST}"
