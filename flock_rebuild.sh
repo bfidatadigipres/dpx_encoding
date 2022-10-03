64e1b0f2ae38a540302ebcf515c61b07ff9ea95c
digitensions <pittshick@me.com>
2022-10-03 18:08:50 +0100

    Update flock_rebuild script

---
+++ flock_rebuild.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#############################################
+# CHECK IF FLOCK FILES EXIST, IF NOT RECREATE
+#############################################
+
+LOCKS=( "/var/run/dpx_assess1.lock"
+        "/var/run/dpx_assess2.lock"
+        "/var/run/dpx_assess3.lock"
+        "/var/run/dpx_assess4.lock"
+        "/var/run/dpx_assess5.lock"
+        "/var/run/dpx_rawcook1.lock"
+        "/var/run/dpx_rawcook2.lock"
+        "/var/run/dpx_rawcook3.lock"
+        "/var/run/dpx_rawcook4.lock"
+        "/var/run/dpx_rawcook5.lock"
+        "/var/run/dpx_post_rawcook1.lock"
+        "/var/run/dpx_post_rawcook2.lock"
+        "/var/run/dpx_post_rawcook3.lock"
+        "/var/run/dpx_post_rawcook4.lock"
+        "/var/run/dpx_post_rawcook5.lock"
+        "/var/run/dpx_tar_script1.lock"
+        "/var/run/dpx_tar_script2.lock"
+        "/var/run/dpx_tar_script3.lock"
+        "/var/run/dpx_tar_script4.lock"
+        "/var/run/dpx_tar_script5.lock"
+        "/var/run/dpx_clean_up1.lock"
+        "/var/run/dpx_clean_up2.lock"
+        "/var/run/dpx_clean_up3.lock"
+        "/var/run/dpx_clean_up4.lock"
+        "/var/run/dpx_clean_up5.lock"
+        "/var/run/dpx_check_script1.lock"
+        "/var/run/dpx_check_script2.lock"
+        "/var/run/dpx_check_script3.lock"
+        "/var/run/dpx_check_script4.lock"
+        "/var/run/dpx_check_script5.lock"
+)
+
+for lock in "${LOCKS[@]}" ; do
+    if [[ -f "$lock" ]];then
+        true
+    else
+        sudo touch "$lock"
+    fi
+done
