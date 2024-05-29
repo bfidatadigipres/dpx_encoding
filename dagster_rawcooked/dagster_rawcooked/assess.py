7651079163362dd7d9a9d57123de10e96bfc8118
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-05-29 16:56:50 +0100

    Additions for assess.py

--- dagster_rawcooked/dagster_rawcooked/assess.py
+++ dagster_rawcooked/dagster_rawcooked/assess.py
@@ -22,7 +22,9 @@ import datetime
 from ds3 import ds3, ds3Helpers
 from dagster import asset, DynamicOut, DynamicOutput
 import dpx_assess
-from .config import DOWNTIME, DATABASE, QNAP_FILM, ASSESS, DPX_COOK, MKV_ENCODED, DPOLICY
+import sqlite_funcs
+import dpx_seq_gap_check
+from .config import DOWNTIME, DATABASE, QNAP_FILM, ASSESS, DPX_COOK, MKV_ENCODED, DPOLICY, DPX_REVIEW
 
 CLIENT = ds3.createClientFromEnv()
 HELPER = ds3Helpers.Helper(client=CLIENT)
@@ -81,21 +83,47 @@ def assessment(context, dynamic_process_subfolders):
     ''' Calling dpx_assess modules run assessment '''
     dpx_path = dynamic_process_subfolders
     dpx_seq = os.path.split(dpx_path)
-    cook_path = os.path.join(QNAP_FILM, DPX_COOK, dpx_seq)
     context.log.info(f"Processing DPX sequence: {dpx_path}")
 
     part, whole = dpx_assess.get_partwhole(dpx_seq)
     context.log.info(f"* Reel number {str(part).zfill(2)} of {str(whole).zfill(2)}")
-    size, cspace, bitdepth, encode_type = dpx_assess.get_metadata(dpx_path)
-    context.log.info(f"* Size: {size} | Colourspace {cspace} | Bit-depth {bitdepth} | Encode type {encode_type}")
+    gaps, missing = dpx_seq_gap_check.gaps(dpx_path)
+    if gaps is True:
+        context.log.info(f"Gaps found in sequence,moving to dpx_review folder: {missing}")
+        review_path = os.path.join(QNAP_FILM, DPX_REVIEW, dpx_seq)
+        shutil.move(dpx_path, review_path)
+        sqlite_funcs.update_table(dpx_seq, f'Gaps found in sequence: {missing}.', DATABASE)
+        return {"status": "gaps", "dpx_seq": dpx_path}
+
+    size, cspace, bitdepth = dpx_assess.get_metadata(dpx_path)
+    context.log.info(f"* Size: {size} | Colourspace {cspace} | Bit-depth {bitdepth}")
     policy_pass = dpx_assess.mediaconch(dpx_path, DPOLICY)
     context.log.info(f"* DPX policy status: {policy_pass}")
     if not policy_pass:
         context.log.info(f"DPX sequence {dpx_seq} failed DPX policy: {dpx_path}")
         # Move to tar wrap into splitting if needed
         # Exit
-    pass
+    else:
+        return {"status": "split rawcook", "dpx_seq": dpx_path, "size": size}
 
+
+@asset
+def handle_assessments(assessment):
+    ''' Move to splittig or to encoding'''
+    if assessment['status'] ==  'gaps':
+        pass
+    elif assessment['status'] == 'tar':
+        # Move to tar wrap path
+        pass
+    elif assessment['status'] == 'rawcook':
+        # Move to RAWcooked path
+        pass
+    elif assessment['status'] == 'split tar':
+        # Initiate splitting with tar argument
+        pass
+    elif assessment['status'] == 'split rawcook'
+        # Initiate splitting with rawcook argument
+        pass
 
 @asset
 def splitting(context, dpx_path):
