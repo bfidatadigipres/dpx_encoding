8bd77ce342dccce0356e6033c0be40ae4eb98875
digitensions <pittshick@me.com>
2024-05-29 16:58:09 +0100

    Serveer updates to launch dev

--- dagster_rawcooked/dagster_rawcooked/__init__.py
+++ dagster_rawcooked/dagster_rawcooked/__init__.py
@@ -1,8 +1,8 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import assets
+from . import assess
 
-all_assets = load_assets_from_modules([assets])
+all_assets = load_assets_from_modules([assess])
 
 defs = Definitions(
     assets=all_assets,
