a63baf2e201f64df85fb46d47d029694091fe688
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-05-30 16:32:47 +0100

    Arrange folders

--- dagster_rawcooked/dagster_rawcooked/__init__.py
+++ dagster_rawcooked/dagster_rawcooked/__init__.py
@@ -1,9 +1,8 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import assets_assess
-from . import assets_rawcook
+from . import assets_assess, assets_rawcook
 
-all_assets = load_assets_from_modules([assets_assess], [assets_rawcook])
+all_assets = load_assets_from_modules([assets_assess, assets_rawcook])
 
 defs = Definitions(
     assets=all_assets,
