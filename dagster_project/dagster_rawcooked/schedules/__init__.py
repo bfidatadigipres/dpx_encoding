80b849594f141d3fbcff236a838e1e96dcc9d1aa
Joanna White <37188631+digitensions@users.noreply.github.com>
2025-02-12 16:12:23 +0000

    Remove old dagster project

--- dagster_project/dagster_rawcooked/schedules/__init__.py
+++
@@ -1,7 +0,0 @@
-from dagster import ScheduleDefinition
-from ..jobs import rawcooked_start_jobs
-
-rawcooked_schedule = ScheduleDefinition(
-    job=rawcooked_start_jobs,
-    cron_schedule = "*/15 * * * *",
-)
