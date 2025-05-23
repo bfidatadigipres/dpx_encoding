97a0d463ab15475eb311df5ca717fa58ada16887
digitensions <pittschick@me.com>
2025-05-23 16:01:07 +0100

    Add checks

--- bfi_dagster_project/sensors/__init__.py
+++ bfi_dagster_project/sensors/__init__.py
@@ -60,6 +60,7 @@ def build_failed_encoding_retry_sensor(key_prefix: Optional[str] = None):
             spath = seq[3]
             key = seq[28]
             if key_prefix != key:
+                context.log.info(f"Key prefix {key_prefix} does not match sequence {key}")
                 continue
             retry_count = seq[17]
             if not retry_count.isnumeric():
