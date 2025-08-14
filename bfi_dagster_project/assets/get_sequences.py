4bc515d2b9452b4baf163670469d27980aeb0046
Joanna White <37188631+digitensions@users.noreply.github.com>
2025-08-14 10:50:06 +0100

    Make if/elif more explicit

--- bfi_dagster_project/assets/get_sequences.py
+++ bfi_dagster_project/assets/get_sequences.py
@@ -68,16 +68,13 @@ def build_target_sequences_asset(key_prefix: Optional[str] = None):
                     result,
                 )
                 continue
-            elif (
-                len(result) > 0
-                and "Triggered assessment" in str(result)
-                and "Accept gaps" in str(result)
-            ):
-                context.log.info(
-                    f"{log_prefix}Picking up sequence a second time. Passing for processing: %s",
-                    result,
-                )
-                current_files.append(f"GAPS_{dpath}")
+            elif len(result) > 0 and "Accept gaps" in str(result):
+                if "Triggered assessment" in str(result):
+                    context.log.info(
+                        f"{log_prefix}Picking up sequence a second time. Passing for processing with accept gaps: %s",
+                        result,
+                    )
+                    current_files.append(f"GAPS_{dpath}")
             elif len(result) > 0 and "Triggered assessment" in str(result):
                 context.log.info(
                     f"{log_prefix}Picking up sequence a second time. Passing for processing: %s",
